b8f6c740ae77ecc60cf982e860fb4039e04d1d58
FishyFoshy <157636316+FishyFoshy@users.noreply.github.com>
2025-09-29 01:58:31 -0400

    Added admin and staff models

---
+++ App/models/staff.py
@@ -0,0 +1,25 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from App.database import db
+
+class Staff(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name =  db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    shifts = db.relationship('Shift', backref="staff")
+
+    def __init__(self, name, password):
+        self.name = name
+        self.set_password(password)
+
+    def get_json(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'shifts': [shift.get_json() for shift in self.Shifts]
+        }
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
